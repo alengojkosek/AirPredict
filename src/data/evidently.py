e2537b9fe126cb691f6034268ef8c918bc14ec28
alengojkosek <alen.gojkosek@student.um.si>
2023-03-29 00:30:20 +0200

    Move data

--- src/data/evidently.py
+++ src/data/evidently.py
@@ -1,35 +1,31 @@
-import evidently
 import pandas as pd
 import numpy as np
-
-
 from evidently.report import Report
-from evidently.metrics.base_metric import generate_column_metrics
-from evidently.metric_preset import DataDriftPreset, TargetDriftPreset, DataQualityPreset, RegressionPreset
-from evidently.metrics import *
-
+from evidently.metrics import base_metrics, regression_metrics
+from evidently.metric import DataDrift, TargetDrift, DataQuality
+from evidently.presets import DataDriftPreset, TargetDriftPreset, DataQualityPreset, RegressionPreset
 from evidently.test_suite import TestSuite
-from evidently.tests.base_test import generate_column_tests
-from evidently.test_preset import DataStabilityTestPreset, NoTargetPerformanceTestPreset, RegressionTestPreset
-from evidently.tests import *
-
+from evidently.tests.base import generate_tests
+from evidently.test import DataStabilityTest, NoTargetPerformanceTest, RegressionTest
 import warnings
+
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 
-data = pd.read_csv('current_data.csv', as_frame=True)
+data = pd.read_csv('./data/processed/current_data.csv')
 
-airpredict_data = data.frame
-
+airpredict_data = data.copy()
 airpredict_data.rename(columns={'pm10': 'target'}, inplace=True)
 airpredict_data['prediction'] = airpredict_data['target'].values + np.random.normal(0, 5, airpredict_data.shape[0])
 
 reference = airpredict_data.sample(n=5000, replace=False)
 current = airpredict_data.sample(n=5000, replace=False)
 
-report = Report(metrics=[
-    DataDriftPreset(),
-])
+report = Report(
+    [
+        DataDrift(),
+    ]
+)
 
 report.run(reference_data=reference, current_data=current)
-report.save_html("evidently_report.html")
+report.save("evidently_report.html")
