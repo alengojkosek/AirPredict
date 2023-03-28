58c1aabf0ca15afc3f0ce099970997cd65f5e629
alengojkosek <alen.gojkosek@student.um.si>
2023-03-29 00:41:54 +0200

    Evidently

--- src/data/evidently.py
+++ src/data/evidently.py
@@ -10,7 +10,11 @@ data = pd.read_csv('./data/processed/current_data.csv')
 
 airpredict_data = data.copy()
 airpredict_data.rename(columns={'pm10': 'target'}, inplace=True)
+
+airpredict_data = airpredict_data.replace('<2', np.nan)
+airpredict_data = airpredict_data.replace('<1', np.nan)
 airpredict_data['target'] = airpredict_data['target'].astype(float)
+airpredict_data['target'] = airpredict_data['target'].fillna(df['pm10'].mean())
 airpredict_data['prediction'] = airpredict_data['target'] + np.random.normal(0, 5, airpredict_data.shape[0])
 
 reference = airpredict_data.sample(n=5000, replace=False)
