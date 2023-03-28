70cd65f9b0017a56a5eec59d56dc1a1062350583
alengojkosek <alen.gojkosek@student.um.si>
2023-03-29 00:58:22 +0200

    Evidently_file

--- src/data/evidently_file.py
+++ src/data/evidently_file.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import numpy as np
-import evidently
-from evidently import Report
-import warnings
 
+from evidently import ColumnMapping
+
+from evidently.report import Report
+from evidently.metrics.base_metric import generate_column_metrics
+from evidently.metric_preset import DataDriftPreset, TargetDriftPreset
+from evidently.metrics import *
+
+from evidently.test_suite import TestSuite
+from evidently.tests.base_test import generate_column_tests
+from evidently.test_preset import DataStabilityTestPreset, NoTargetPerformanceTestPreset
+from evidently.tests import *
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 
