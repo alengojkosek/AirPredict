01e7fd9258754aea0a3cef8c7305a06592d92203
alengojkosek <alen.gojkosek@student.um.si>
2023-03-30 00:06:33 +0200

    Mlflow

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,10 +6,10 @@ import pickle
 import mlflow
 
 
-MLFLOW_TRACKING_URI='https://dagshub.com/alengojkosek/AirPredict.mlflow'
+MLFLOW_TRACKING_URI="https://dagshub.com/alengojkosek/AirPredict.mlflow"
 
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-mlflow.set_experiment('AirPredict')
+mlflow.set_experiment("/AirPredict")
 
 mlflow.autolog(exclusive=False)
 
