d0de3bd86c9804c031be62725ed4d469a25322ca
alengojkosek <alen.gojkosek@student.um.si>
2023-03-30 01:40:47 +0200

    Mlflow

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -11,61 +11,61 @@ mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.autolog(exclusive=False)
 
 
+mlflow.start_run()
 
-with mlflow.start_run():
-    # Load the dataset
-    df = pd.read_csv('merged_processed_data.csv')
-    df = df.drop(columns=['sifra','o3', 'benzen', 'co', 'no2', 'so2', 'Unnamed: 0','ge_dolzina','ge_sirina','pm2.5', 'nadm_visina','time', 'merilno_mesto', 'datum_od', 'datum_do', 'Unnamed: 0_y', 'Unnamed: 0_x'])
-    df = df.replace('<2', np.nan)
-    df = df.replace('<1', np.nan)
+# Load the dataset
+df = pd.read_csv('merged_processed_data.csv')
+df = df.drop(columns=['sifra','o3', 'benzen', 'co', 'no2', 'so2', 'Unnamed: 0','ge_dolzina','ge_sirina','pm2.5', 'nadm_visina','time', 'merilno_mesto', 'datum_od', 'datum_do', 'Unnamed: 0_y', 'Unnamed: 0_x'])
+df = df.replace('<2', np.nan)
+df = df.replace('<1', np.nan)
 
-    df = df.interpolate()
-    df = df.dropna()
+df = df.interpolate()
+df = df.dropna()
 
-    x = df.drop('pm10', axis=1)
-    y = df['pm10']
+x = df.drop('pm10', axis=1)
+y = df['pm10']
 
-    # Split the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-    model = LinearRegression()
+model = LinearRegression()
 
-    model.fit(X_train, y_train)
+model.fit(X_train, y_train)
 
-    # Save the model to disk
-    with open('LG_model_4.pkl', 'wb') as f:
-        pickle.dump(model, f)
+# Save the model to disk
+with open('LG_model_4.pkl', 'wb') as f:
+    pickle.dump(model, f)
 
-    # Use the model to make predictions on the training data
-    y_pred_train = model.predict(X_train)
+# Use the model to make predictions on the training data
+y_pred_train = model.predict(X_train)
 
-    # Evaluate the performance of the model on the training data
-    train_score = model.score(X_train, y_train)
-    print(f"Training R-squared score: {train_score:.2f}")
+# Evaluate the performance of the model on the training data
+train_score = model.score(X_train, y_train)
+print(f"Training R-squared score: {train_score:.2f}")
 
-    # Calculate mean squared error (MSE) on the testing data
-    mse = mean_squared_error(y_train, y_pred_train)
-    print(f"Training MSE: {mse:.2f}")
+# Calculate mean squared error (MSE) on the testing data
+mse = mean_squared_error(y_train, y_pred_train)
+print(f"Training MSE: {mse:.2f}")
 
-    # Calculate mean absolute error (MAE) on the testing data
-    mae = mean_absolute_error(y_train, y_pred_train)
-    print(f"Training MAE: {mae:.2f}")
+# Calculate mean absolute error (MAE) on the testing data
+mae = mean_absolute_error(y_train, y_pred_train)
+print(f"Training MAE: {mae:.2f}")
 
-    # Use the model to make predictions on the testing data
-    y_pred_test = model.predict(X_test)
+# Use the model to make predictions on the testing data
+y_pred_test = model.predict(X_test)
 
-    # Evaluate the performance of the model on the testing data
-    test_score = model.score(X_test, y_test)
-    print(f"Testing R-squared score: {test_score:.2f}")
+# Evaluate the performance of the model on the testing data
+test_score = model.score(X_test, y_test)
+print(f"Testing R-squared score: {test_score:.2f}")
 
-    # Calculate mean squared error (MSE) on the testing data
-    mse = mean_squared_error(y_test, y_pred_test)
-    print(f"Testing MSE: {mse:.2f}")
+# Calculate mean squared error (MSE) on the testing data
+mse = mean_squared_error(y_test, y_pred_test)
+print(f"Testing MSE: {mse:.2f}")
 
-    # Calculate mean absolute error (MAE) on the testing data
-    mae = mean_absolute_error(y_test, y_pred_test)
-    print(f"Testing MAE: {mae:.2f}")
+# Calculate mean absolute error (MAE) on the testing data
+mae = mean_absolute_error(y_test, y_pred_test)
+print(f"Testing MAE: {mae:.2f}")
 
-    mlflow.log_metric("MAE", mae)
-    mlflow.log_metric("MSE", mse)
-    mlflow.log_metric("Train score", train_score)
+mlflow.log_metric("MAE", mae)
+mlflow.log_metric("MSE", mse)
+mlflow.log_metric("Train score", train_score)
